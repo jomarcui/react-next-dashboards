cfc7ad0f063afa27981632008561cc2eafb23c9b
jomarcui <cuijomar@gmail.com>
2022-06-26 12:39:13 +0800

    💄 Google font fix

--- dashboard-01/pages/_document.tsx
+++ dashboard-01/pages/_document.tsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700,900&display=optional"
+            href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700;900&display=optional"
             rel="stylesheet"
           />
           <meta
